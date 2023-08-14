b1d3f5cbcf14e5118047bab60e03350a644bef3d
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2023-08-14 13:03:03 -0400

    Use ponder's native contract reading
    Same functionality but implements caching for performance improvements on restarts

--- src/ERC1967Proxy.ts
+++ src/ERC1967Proxy.ts
@@ -1,20 +1,10 @@
 import { ponder } from "@/generated";
-import { bytesToString, parseAbi, toBytes } from "viem";
-import { client } from "./client";
-
-const abi = parseAbi(['function idOf(address) view returns (uint256)']);
-const address = '0xda107a1caf36d198b12c16c7b6a1d1c795978c42';
+import { bytesToString, toBytes } from "viem";
 
 ponder.on("ERC1967Proxy:Transfer", async ({ event, context }) => {
   const { Fname } = context.entities;
 
-  const fid = await client.readContract({
-    abi,
-    address,
-    functionName: 'idOf',
-    args: [event.params.to],
-    blockNumber: event.block.number,
-  });
+  const fid = await context.contracts.IdRegistry.read.idOf([event.params.to])
  
   const fname = await Fname.upsert({
     id: event.params.tokenId,
--- src/client.ts
+++
@@ -1,7 +0,0 @@
-import { createPublicClient, http } from 'viem';
-import { goerli } from 'viem/chains';
-
-export const client = createPublicClient({ 
-  chain: goerli,
-  transport: http(process.env.PONDER_RPC_URL_5),
-});
