de57218bbf7aa21c322e75109ae9bd0365cf01be
Varun Srinivasan <varunsrin@gmail.com>
2023-08-13 19:10:53 -0700

    feat: expose fnames as strings

--- src/ERC1967Proxy.ts
+++ src/ERC1967Proxy.ts
@@ -1,15 +1,23 @@
 import { ponder } from "@/generated";
+import { bytesToString, toBytes } from "viem";
 
 ponder.on("ERC1967Proxy:Transfer", async ({ event, context }) => {
-  const { Username } = context.entities;
+  const { Fname } = context.entities;
  
-  const user = await Username.upsert({
+  const fname = await Fname.upsert({
     id: event.params.tokenId,
     create: {
       address: event.params.to,
+      fname: fnameFromTokenId(event.params.tokenId),
     },
     update: {
       address: event.params.to,
     },
   });
 });
+
+
+// Converts ERC-721 tokenId bigint into Fname string
+export const fnameFromTokenId = (tokenId: bigint) => {
+  return bytesToString(toBytes(tokenId)).replace(/\0/g, '');
+};
